bd90f19be6344d1ecb9b3860426ed56058ebb0f5
Samincode01 <samin990bd@gmail.com>
2026-01-25 00:10:00 +0600

    added another code for bmi

---
+++ bmi_calculator.js
@@ -0,0 +1,17 @@
+var BMI = 22
+if(BMI < 18.5)
+{
+    console.log("you are underweight")
+}
+else if(BMI >= 18.5 && BMI <=24.9)
+{
+    console.log("you are normal")
+}
+else if(BMI >=25 and BMI <= 29.9)
+{
+    console.log("you are overweight")
+}
+else
+{
+    console.log("you are obese")
+}
