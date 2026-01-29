b14328a18436116fdab3927f0acd1c9bb3b4968a
Samincode01 <samin990bd@gmail.com>
2026-01-29 23:48:57 +0600

    Updated object codes

--- object.js
+++ object.js
@@ -9,3 +9,10 @@ console.log(person.name)
 console.log(person.age)
 console.log(person.profession)
 console.log(person.salary)
+
+const keys = Object.keys(person)
+console.log(keys)
+
+const values = Object.values(person)
+console.log(values)
+
