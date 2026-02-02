0d81a9794e61e107f0c5002f36bbb698f31d5404
Samincode01 <samin990bd@gmail.com>
2026-02-02 23:36:37 +0600

    updated codes

---
+++ case.js
@@ -0,0 +1,13 @@
+const subject='Chemistry'
+const book='Chemistry'
+console.log(subject)
+console.log(book)
+
+if(subject.toLowerCase()==subject.toLowerCase())
+{
+    console.log('I am reading this book')
+}
+else
+{
+    console.log('I am not reading this book')
+}
---
+++ concat.js
@@ -0,0 +1,10 @@
+const firstname = 'David';
+const lastname = 'Jones';
+const fullname = firstname + lastname;
+console.log(fullname)
+
+const fullName = firstname + ' ' + lastname;
+console.log(fullName);
+
+const fullName2 = firstname.concat(' ').concat(lastname)
+console.log(fullName2);
